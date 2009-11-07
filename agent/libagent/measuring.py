e6207ab5ca038b2106424f176943df682a3cd849
ivo <ivo@danihelka.net>
2009-11-07 15:37:19 +0100

    Measured avg load and the number of processes.

--- agent/libagent/measuring.py
+++ agent/libagent/measuring.py
@@ -1,4 +1,21 @@
 
 def measure_stats():
-    #TODO: implement
-    return {}
+    return {
+            'loadAvrg': _measure_load(),
+            'processesCnt': _measure_num_processes(),
+            }
+
+def _measure_load():
+    loadavg = open('/proc/loadavg').readline()
+    over5minutes = float(loadavg.split()[1])
+    return over5minutes
+
+def _measure_num_processes():
+    output = _capture('ps', 'ax')
+    return len(output.splitlines()) - 1
+
+def _capture(*args):
+    import subprocess
+    process = subprocess.Popen(args, stdout=subprocess.PIPE, close_fds=True)
+    return process.communicate()[0]
+
