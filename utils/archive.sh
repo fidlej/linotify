c11b315a04aca54cd1e2e4e61e355f1b6a10dc80
ivo <ivo@danihelka.net>
2009-11-07 17:13:21 +0100

    Updated the usage HOWTO.

--- utils/archive.sh
+++ utils/archive.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+# Archives the HEAD version of linotify-agent.
 set -e
 
-rm -rf release/linotify
+rm -rf release
 mkdir -p release/linotify
 hg archive release/linotify
-tar czf release/linotify-agent.tar.gz release/linotify/agent
+mv release/linotify/agent release/linotify-agent
+tar czf release/linotify-agent.tar.gz -C release linotify-agent
+
 mv release/linotify-agent.tar.gz app/static/download/
-tar czf release/linotify.tar.gz release/linotify
-mv release/linotify.tar.gz app/static/download/
+rm -rf release
+
