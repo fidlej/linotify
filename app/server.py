54c5dd07448f767c339b7ad125e3f4f8a2883121
ivo <ivo@danihelka.net>
2009-11-09 13:56:37 +0100

    Disabled the profiling for now.

--- app/server.py
+++ app/server.py
@@ -128,9 +128,8 @@ app = webapp.WSGIApplication(
         ],
         debug=config.DEBUG)
 
-from src.profiling import profiled
-
-@profiled
+#from src.profiling import profiled
+#@profiled
 def main():
     wsgiref.handlers.CGIHandler().run(app)
 
