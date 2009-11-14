f3530432f2651d17de5bf37d463402c1956fb183
ivo <ivo@danihelka.net>
2009-11-14 21:45:09 +0100

    Listed the sha1sums of the installed files.

--- utils/archive.sh
+++ utils/archive.sh
@@ -6,8 +6,10 @@ rm -rf release
 mkdir -p release/linotify
 hg archive release/linotify
 mv release/linotify/agent release/linotify-agent
+find release/linotify-agent/ -type f -exec sha1sum {} \; | sed 's@  release/linotify-agent/@  @' >release/files.sha1
+mv release/files.sha1 release/linotify-agent
 tar czf release/linotify-agent.tar.gz -C release linotify-agent
 
 mv release/linotify-agent.tar.gz app/static/download/
-rm -rf release
+#rm -rf release
 
