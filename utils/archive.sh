15da8080de1f31bb1a5e7e56e6f6e9c0b24347d7
ivo <ivo@danihelka.net>
2009-11-16 12:36:24 +0100

    Named the file files.sha1sum.

--- utils/archive.sh
+++ utils/archive.sh
@@ -7,8 +7,8 @@ mkdir -p release/linotify
 hg archive release/linotify
 mv release/linotify/agent release/linotify-agent
 rm -rf 'release/linotify-agent/test'
-find release/linotify-agent/ -type f -exec sha1sum {} \; | sed 's@  release/linotify-agent/@  @' >release/files.sha1
-mv release/files.sha1 release/linotify-agent
+find release/linotify-agent/ -type f -exec sha1sum {} \; | sed 's@  release/linotify-agent/@  @' >release/files.sha1sum
+mv release/files.sha1sum release/linotify-agent
 tar czf release/linotify-agent.tar.gz -C release linotify-agent
 
 mv release/linotify-agent.tar.gz app/static/download/
