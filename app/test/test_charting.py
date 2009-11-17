c01b42b184df6e901a36ccdd5d11ae0ea6984b6f
ivo <ivo@danihelka.net>
2009-11-17 23:20:54 +0100

    Added a test for the fixed chart definition.

---
+++ app/test/test_charting.py
@@ -0,0 +1,15 @@
+
+from nose.tools import assert_equal, assert_not_equal
+from src import charting
+
+def test_fixed_chart():
+    chart = charting.FixedChart(u'A chart',
+            ('key1', 'key2'),
+            (
+                {'option1':'o1', 'option2':'o2'},
+                {'option1':'key2_o1', 'option2':'key2_o2'}))
+
+    assert_equal(chart.is_interesting('key1'), True)
+    assert_equal(chart.is_interesting('key_x'), False)
+    assert_equal(chart.get_options(0, 'key1')['option1'], 'o1')
+
