c1bac6804ed36218c1f2a1dbbdb03a3a48c3ca44
ivo <ivo@danihelka.net>
2009-11-22 11:17:54 +0100

    Allowed to choose the timezone.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,7 +3,7 @@ var setCookie = function(name, value, days) {
     var date = new Date();
     date.setTime(date.getTime() + days*24*3600*1000);
     var expires = "; expires=" + date.toUTCString();
-    document.cookie = [name, "=", encodeURIComponent(value), expires, "; path=/"].join('');
+    document.cookie = [name, "=", window.encodeURIComponent(value), expires, "; path=/"].join('');
 };
 
 var getCookie = function(name) {
@@ -22,10 +22,29 @@ var getCookie = function(name) {
     return undefined;
 }
 
+var prepareTzSelect = function(selectId) {
+    var select = document.getElementById(selectId);
+    if (!select) {
+        return;
+    }
+
+    select.onchange = function() {
+        var tz = select.value;
+        if (tz != 60 * new Date().getTimezoneOffset()) {
+            // preserve it
+            tz = tz + 'p';
+        }
+        setCookie('tz', tz, 365);
+        window.location.reload(true);
+    }
+}
+
 // Don't overwrite the tz value if user wants it to be preserved.
 var tz = getCookie('tz');
 if (!/p$/.test(tz)) {
     tz = new Date().getTimezoneOffset();
 }
 setCookie('tz', tz, 365);
+prepareTzSelect('tz_select');
+
 })();
